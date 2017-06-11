61d61ac43dde5a3d7c2256be16894ee9dc0046c5
JordyRG21 <jordyromgu@hotmail.com>
2017-06-10 18:24:57 -0600

    Pantalla de inicio

--- app/src/main/java/una/ac/cr/spaceinvaders/Inicio.java
+++ app/src/main/java/una/ac/cr/spaceinvaders/Inicio.java
@@ -1,11 +1,21 @@
 package una.ac.cr.spaceinvaders;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Inicio extends AppCompatActivity {
+
+
+    Button botonInicio;
+    Button botonFinal;
+    TextView txtTitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,31 @@ public class Inicio extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        //Para calcular las coordenadas maximas de nuestro dispositivo
+        Display display = getWindowManager().getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+
+        botonInicio = (Button) findViewById(R.id.buttonInicio);
+        botonFinal = (Button) findViewById(R.id.buttonFinal);
+        txtTitulo = (TextView) findViewById(R.id.txtTitulo);
+
+        txtTitulo.setX(point.x/3);
+
+        botonInicio.setX(point.x/6);
+        botonInicio.setY(point.y/3);
+
+        botonFinal.setX(point.x/2+120);
+        botonFinal.setY(point.y/3);
+    }
+
+    public void Iniciar(View view) {
+
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+
     }
 }
